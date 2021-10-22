4ee4e06a47cbac12f36932997ea90996e81c4132
CyannRogue <cyanroger22@outlook.com>
2021-10-22 05:50:44 +0200

    addad JS on UI V1

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-locate();
+locateAndWeather();
 ///////////////////////////////////
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
